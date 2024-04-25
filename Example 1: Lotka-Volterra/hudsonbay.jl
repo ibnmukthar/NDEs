20be972028793493ac0a1cb14b716af9564a5a5f
Muhammad Mukthar <muhammad.mukthar11@gmail.com>
2024-04-25 17:56:35 +0800

    Update hudsonbay.jl

--- Example 1: Lotka-Volterra/hudsonbay.jl
+++ Example 1: Lotka-Volterra/hudsonbay.jl
@@ -1,4 +1,4 @@
-'''
+#=
 MIT License
 
 Copyright (c) 2020 Christopher Rackauckas
@@ -20,7 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-'''
+=#
 ## Environment and packages
 cd(@__DIR__)
 using Pkg; Pkg.activate("."); Pkg.instantiate()
